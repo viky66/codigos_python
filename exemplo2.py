0df80ee46ab0dfd50fe9dc69cd45dbbe258c0081
Victória Regina <ribeiromoreira7341@gmail.com>
2024-09-03 09:46:00 -0300

    Calculando media com python

--- exemplo2.py
+++ exemplo2.py
@@ -2,4 +2,14 @@ nome = input("Informe seu nome: ")
 end = input("Informe seu endereço: ")
 idade = input("Informe sua idade: ")
 
+# Exibindo dados das variáveis
 print(nome, end, idade)
+
+#1 forma de concatenação
+print("\nOlá ",nome, " seu endereço é ", end," sua idade é ", idade)
+
+#2 forma de concatenação
+print("\nOlá {} seu endereço é {} e sua idade é {}".format(nome, end,idade))
+
+#3 forma de concatenação - f string
+print(f"Bem Vind@ {nome}, você mora na {end} e tem {idade} anos ")
