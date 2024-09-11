2e9f3bb2a082a89f62ef3f9ec4ad2418d2e8b61e
Victória Oi miga Luddd <ribeiromoreira7341@gmail.com>
2024-09-11 11:09:23 -0300

    Atividades Repetição Para

--- atividade repetição para/atvd4.py
+++ atividade repetição para/atvd4.py
@@ -1,7 +1,7 @@
 numero = []
 for i in range(5):
     numeros = int(input(f"digite o número {i+1}: "))
-    numero.append
+    numero.append(numeros)
 
 maior = numeros
 print(f"o maior número informado é: {maior}")
