dd958ed9317739b5ceb2432937b7f0f6e6e8e718
Victória Oi miga Luddd <ribeiromoreira7341@gmail.com>
2024-09-11 11:06:52 -0300

    Atividades Repetição Para

--- atividade repetição para/atvd6.py
+++ atividade repetição para/atvd6.py
@@ -1,13 +1,13 @@
-n = int(input("Digite um número inteiro: "))
+num = int(input("Digite um número inteiro: "))
 
 soma_div = 0
 
-for i in range(1, n):
-    if n % i == 0:
+for i in range(1, num):
+    if num % i == 0:
         soma_div += i
     
 # Verifica se o número é perfeito e exibe o resultado
-if soma_div == n:
-    print(f"O número {n} é um número perfeito.")
+if soma_div == num:
+    print(f"O número {num} é um número perfeito.")
 else:
-    print(f"O número {n} não é um número perfeito.")
+    print(f"O número {num} não é um número perfeito.")
